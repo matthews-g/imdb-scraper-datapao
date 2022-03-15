68c6a8281099f630c77ad6e01dab5ffa3c799b09
Mate Geczi <mategeczi@gmail.com>
2022-03-15 13:56:52 +0100

    Sample usage provided

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from datapao_scraper import Scraper
 from datapao_manipulator import DataManipulator
+import pandas as pd
 
-toptwenty = Scraper(20)
-toptwenty.full_task()
-print(toptwenty.movie_dataframe)
+if __name__ == '__main__':
+    TOP_MOVIES = 20
 
-toptwenty_manipulator = DataManipulator(toptwenty.movie_dataframe)
-toptwenty_manipulator.full_task()
-print(toptwenty_manipulator.movie_dataframe)
+    top_scraper = Scraper(TOP_MOVIES)
+    top_scraper.full_task()
+
+    top_manipulator = DataManipulator(top_scraper.movie_dataframe)
+    top_manipulator.full_task()
+
+    # READ THE DATAFRAMES FROM THE SAVED JSON FILES
+
+    original_dataframe = pd.read_json(top_manipulator.movie_dataframe_org_name)
+    modified_dataframe = pd.read_json(top_manipulator.movie_dataframe_name)
+
+    print("\n")
+    print("Original dataframe below:")
+    print(original_dataframe)
+    print("\n")
+    print("Modified dataframe below:")
+    print(modified_dataframe)
+    print("\n")
+    input("PRESS ENTER TO EXIT")
+    input("PRESS ENTER TO EXIT")
 
